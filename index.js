198deb09f00a049ff1bffcd217f925c7bb760456
nilssvensson <nils27school@gmail.com>
2022-09-02 13:15:49 -0400

    commit tree 509b26c4bb36d8b353ad303d041488047850afe1

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// let num1 = 31
+// let num2 = 2
+// const multiply =  num1 * num2
 
-// Write your code here
+// let random = Math.random() * 10
+// console.log(random)
+
+// let num3 = 16
+// let num4 = 4
+// const mod = num3 / num4
+
+// Math.max(1, 2, 3, 4, 20);
+// console.log(Math.max);
+
+// const max = Math.floor(Math.random() * 10);
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 1000 ) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
